55d48e7bc049f77eb9e855f7ac990109cad2678a
Gerardo N <galmazan0951@gmail.com>
2024-09-17 06:16:24 -0600

    Codigo editado de forma estructurada

--- P1/Program.cs
+++ P1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arregloNumeros = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+int contadorIndicesDuplicados = 0;
+
+Array.Sort(arregloNumeros);
+int numeroMaximo = arregloNumeros[arregloNumeros.Length - 1];
+
+Console.WriteLine("Arreglo de entrada:\n");
+foreach (int numero in arregloNumeros){
+    Console.Write($"{numero} ");
+}
+
+for(int i = 0; i < arregloNumeros.Length ; i++){
+    for(int j = i + 1; j < arregloNumeros.Length; j++){
+        if(arregloNumeros[j] == arregloNumeros[i]){
+            arregloNumeros[j] = ++numeroMaximo;
+            contadorIndicesDuplicados++;
+        }
+    }
+}
+Array.Sort(arregloNumeros);
+Array.Resize(ref arregloNumeros, arregloNumeros.Length - contadorIndicesDuplicados);
+Console.WriteLine("Resultados de salida:\n");
+foreach (int numero in arregloNumeros){
+    Console.Write($"{numero} ");
+}
+Console.WriteLine($"\nk: {arregloNumeros.Length}");
