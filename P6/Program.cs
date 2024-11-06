7a9c7e67e13409620d6f144c3cf404101f8c7f74
Gerald-Alms <galmazan0951@gmail.com>
2024-11-05 21:58:46 -0600

    Adición de código en P6

--- P6/Program.cs
+++ P6/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Ejemplo de prueba
+int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+
+int maximaAgua = MaximaCantidadAgua(height);
+
+public static int MaximaCantidadAgua(int[] alturas){
+    int izquierda = 0;               // Puntero izquierdo
+    int derecha = alturas.Length - 1; // Puntero derecho
+    int maxArea = 0;                 // Máxima área encontrada
+
+    while (izquierda < derecha){
+        // Calcular el área actual
+        int ancho = derecha - izquierda;
+        int alturaActual = Math.Min(alturas[izquierda], alturas[derecha]);
+        int areaActual = ancho * alturaActual;
+
+        // Actualizar el área máxima si es mayor que la anterior
+        maxArea = Math.Max(maxArea, areaActual);
+
+        // Mover el puntero que apunta a la altura menor
+        if (alturas[izquierda] < alturas[derecha]){
+            izquierda++;
+        }
+        else{
+            derecha--;
+        }
+    }
+
+    return maxArea;
+}
+
+Console.WriteLine("La máxima cantidad de agua que se puede contener es: " + maximaAgua);
--- P6/obj/Debug/net8.0/P6.AssemblyInfo.cs
+++ P6/obj/Debug/net8.0/P6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e29c10de6a40f4b0058af7233a0d883f09df0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74385505c51cd17643ad8dcd8824985fe8944ebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("P6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
